0aba81acd461aea85228314d0188c924eab93868
AMAN SHAIKH <amannauf100@gmail.com>
2023-12-23 15:44:54 +0530

    changes made in main.js

--- Responsive-Car-Website/assets/js/main.js
+++ Responsive-Car-Website/assets/js/main.js
@@ -28,7 +28,12 @@ const linkAction = () =>{
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
-
+const scrollHeader = () =>{
+    const header = document.getElementById('header')
+    // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+    if(this.scrollY >= 50)  header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
 
 /*=============== POPULAR SWIPER ===============*/
 
