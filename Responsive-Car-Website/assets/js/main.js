4e7f3c6f86e395bd0c600600c11f85906fec944b
AMAN SHAIKH <amannauf100@gmail.com>
2023-12-24 14:52:34 +0530

    final commit

--- Responsive-Car-Website/assets/js/main.js
+++ Responsive-Car-Website/assets/js/main.js
@@ -105,3 +105,26 @@ const scrollActive = () =>{
 window.addEventListener('scroll', scrollActive)
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+  origin: 'top',
+  distance: '60px',
+  duration: 2500,
+  delay: 400,
+  //reset: true
+})
+
+sr.reveal(`.home__title, .popular__container, .features__img, .featured__filters`)
+sr.reveal(`.home__subtitle`, {delay: 500})
+sr.reveal(`.home__elec`, {delay: 600})
+sr.reveal(`.home__img`, {delay: 800})
+sr.reveal(`.home__car-data`, {delay: 900, interval: 100, origin: 'bottom'})
+sr.reveal(`.home__button`, {delay: 1000, origin: 'bottom'})
+sr.reveal(`.about__group, .offer__data`, {origin: 'left'})
+sr.reveal(`.about__data, .offer__img`, {origin: 'right'})
+sr.reveal(`.features__map`, {delay: 600, origin: 'bottom'})
+sr.reveal(`.features__card`, {interval: 300})
+sr.reveal(`.featured__card, .logos__content, .footer__content`, {interval: 100})
+
+
+
+
